3e23df1d7cf0fe0859b931413331bb67c185a638
Arthur Machado <arthur.gmr@hotmail.com>
2021-08-25 23:54:22 -0300

    create-unitary-test

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase"
+
+let createUserUseCase: CreateUserUseCase;
+let userRepositoryInMemory: InMemoryUsersRepository;
+
+describe("Create User", () => {
+    beforeEach(() => {
+        userRepositoryInMemory = new InMemoryUsersRepository();
+        createUserUseCase = new CreateUserUseCase(userRepositoryInMemory);
+    });
+
+    it("Should be able to create a new user", async () => {
+        const user = await createUserUseCase.execute({
+            name: "User test",
+            email: "teste@finapi.com",
+            password: "12345"
+        });
+
+        expect(user).toHaveProperty("id");
+    });
+
+    it("Should not be able to create a new user with same email", () => {
+        expect(async () => {
+            await createUserUseCase.execute({
+                name: "User test",
+                email: "teste@finapi.com",
+                password: "12345"
+            });
+
+            await createUserUseCase.execute({
+                name: "User test",
+                email: "teste@finapi.com",
+                password: "12345"
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+});
